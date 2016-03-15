f1ae20be8cd252f49d5dd303b08ca907d255bd36
Yahor Hayeuski <di23@users.noreply.github.com>
2016-03-15 23:45:39 +0300

    wrote some checks

--- newNull.jsx
+++ newNull.jsx
@@ -3,26 +3,52 @@
 	Author: Yahor Hayeuski
 	Script for ft-Toolbar
 
-	Create new null layer in active comp. The source item always stays the same,
+	Create new null layer in active comp with brown label. The source item always stays the same,
 	so there is no mess in project panel with many items: "Null 1", "Null 2", etc.
 */
 
 (function () {
 
+// Check if two 2D arrays are equal
+function equalArrs( arr1, arr2 ) {
+	if (arr1.length != arr2.length) {
+		return false;
+	}
+	for (var i = 0, l = arr1.length; i < l; i++) {
+		if (arr1[i] != arr2[i]) {
+			// Don't work with complex types (like objects and arrays)
+			return false;
+		}
+	}
+	return true;
+}
+
 var scriptName = 'newNull';
 
-var itemName = 'null';
-var id;
+var name = 'null';
+var label = 12; // Brown label
 
 // Check for selected comp
 var curComp = app.project.activeItem;
 if ((curComp == null) || !(curComp instanceof CompItem)) {
-	//TODO add try-catch or use alert
-	throw scriptName + ' - error 20:\nThere is no active comp.';
+	alert('There is no active comp.');
 }
 
 // Find id of source null, if there is one
-//TODO add here code
+var id;
+var item;
+for (var i = 1, l = app.project.numItems; i <= l; i++) {
+	item = app.project.item(i);
+	if ((item.name == name) &&
+		(item instanceof FootageItem) &&
+		(item.mainSource instanceof SolidSource) &&
+		equalArrs( item.mainSource.color, [1,1,1] ) &&
+		equalArrs( [item.width,item.height], [100,100] )) {
+
+		id = i;
+		break;
+	}
+}
 
 app.beginUndoGroup('ft-Toolbar - ' + scriptName);
 
@@ -36,8 +62,9 @@ if (id) {
 } else {
 	// if there is no null, create new one
 	newNull = this.objRef.layers.addNull();
-	newNull.source.name = itemName;
+	newNull.source.name = name;
 }
+newNull.label = label;
 
 app.endUndoGroup();
 
