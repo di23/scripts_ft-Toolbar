0d6ab340e0ec614816d8f5c17adc261ae83f9b3d
Yahor Hayeuski <di23@users.noreply.github.com>
2016-03-16 14:08:42 +0300

    Done test variant

--- newControlNull.jsx
+++ newControlNull.jsx
@@ -24,12 +24,13 @@ function equalArrs( arr1, arr2 ) {
 	return true;
 }
 
-var scriptName = 'newNull';
+var scriptName = 'newControlNull';
 
 var name = 'controller';
+var label = 11; // Orange label
 var size = [50,50];
 var sourceName = 'null_' + size[0];
-var label = 11; // Orange label
+var sourceLabel = 12; // Brown label
 
 // Check for selected comp
 var curComp = app.project.activeItem;
@@ -43,12 +44,12 @@ var id;
 var item;
 for (var i = 1, l = app.project.numItems; i <= l; i++) {
 	item = app.project.item(i);
-	if ((item.name == name) &&
-		(item.label == label) &&
+	if ((item.name == sourceName) &&
+		(item.label == sourceLabel) &&
 		(item instanceof FootageItem) &&
 		(item.mainSource instanceof SolidSource) &&
 		equalArrs( item.mainSource.color, [1,1,1] ) &&
-		equalArrs( [item.width,item.height], [100,100] )) {
+		equalArrs( [item.width,item.height], size )) {
 
 		id = i;
 		break;
@@ -67,10 +68,14 @@ if (id) {
 } else {
 	// if there is no null, create new one
 	newNull = curComp.layers.addNull();
-	newNull.source.name = name;
-	newNull.source.label = label;
-	newNull.label = label;
+	newNull.source.width = size[0];
+	newNull.source.height = size[1];
+	newNull.source.name = sourceName;
+	newNull.source.label = sourceLabel;
 }
+newNull.property("ADBE Transform Group").property("ADBE Position").setValue([0,0]);
+newNull.label = label;
+newNull.name = name;
 
 app.endUndoGroup();
 
